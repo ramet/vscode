699e8c7ef988abfc4af42e1e05e991fcaf5dd0c1
Pierre Ramet <pierre.ramet@inria.fr>
2021-09-21 16:19:26 +0200

    fix clean

--- clean-mac.sh
+++ clean-mac.sh
@@ -2,15 +2,18 @@
 
 # status
 echo "Your VS Code settings and cache files (before):"
-du -sch ~/.vscode/ ~/Library/Application\ Support/Code/Cache/ ~/Library/Application\ Support/Code/CachedData/ ~/Library/Application\ Support/Code/User/workspaceStorage ~/.vscode-cpptools/
+du -sch ~/.vscode/ ~/Library/Application\ Support/Code/Cache/ ~/Library/Application\ Support/Code/CachedData/ ~/Library/Application\ Support/Code/Code\ Cache/ ~/Library/Application\ Support/Code/CachedExtensions/ ~/Library/Application\ Support/Code/CachedExtensionVSIXs/ ~/Library/Application\ Support/Code/User/workspaceStorage
 echo
 echo "Cleaning cache files..."
 rm -rf ~/Library/Application\ Support/Code/Cache/*
 rm -rf ~/Library/Application\ Support/Code/CachedData/*
+rm -rf ~/Library/Application\ Support/Code/Code\ Cache/*
+rm -rf ~/Library/Application\ Support/Code/CachedExtensions/*
+rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs/*
 rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/*
-rm -rf ~/.vscode-cpptools/ipch/*
+
 echo
 echo "Your VS Code settings and cache files (after):"
-du -sch ~/.vscode/ ~/Library/Application\ Support/Code/Cache/ ~/Library/Application\ Support/Code/CachedData/ ~/Library/Application\ Support/Code/User/workspaceStorage ~/.vscode-cpptools/
+du -sch ~/.vscode/ ~/Library/Application\ Support/Code/Cache/ ~/Library/Application\ Support/Code/CachedData/ ~/Library/Application\ Support/Code/Code\ Cache/ ~/Library/Application\ Support/Code/CachedExtensions/ ~/Library/Application\ Support/Code/CachedExtensionVSIXs/ ~/Library/Application\ Support/Code/User/workspaceStorage
 echo
 echo "Done!"
